6f0c886ebb146579288f18a55db920856315343b
Filip Horvatinović Rumić <rumixpumix@gmail.com>
2025-04-11 02:19:19 +0200

    Created template for sockets
    First step in multiplayer monopoly.

---
+++ socket/client.py
@@ -0,0 +1,12 @@
+#TODO CREATE A SOCKET CLIENT THAT CONNECTS TO THE SERVER EXCHANGES DATA
+# AND CLOSES THE CONNECTION
+#PURELY FUNCTIONS BASED CLIENT
+import socket
+import time
+import json
+import ssl
+import os
+import sys
+import logging
+import threading
+import traceback
